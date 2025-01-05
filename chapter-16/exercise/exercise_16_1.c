d10aa085c75fb7159b843b015ac8078777379e6e
Md. Sufal Mia <sufal.legend.77@gmail.com>
2025-01-06 00:10:10 +0600

    Update exercise_16_1.c
    Bitwise operation

--- chapter-16/exercise/exercise_16_1.c
+++ chapter-16/exercise/exercise_16_1.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
+//Decimal to binary
 
-void decimalToBinary(int n) {
-    if (n == 0) {
-        printf("0");
-        return;
-    }
+void toBinary(int n)
+{
+    int i, mask, result;
 
-    int binaryNum[32];
-    int i = 0;
-    while (n > 0) {
-        binaryNum[i] = n % 2;
-        n = n / 2;
-        i++;
+    for (i = 15; i >= 0; i--)
+    {
+        mask = 1 << i;
+        result = mask & n;
+        if (result != 0)
+            printf("1");
+        else
+            printf("0");
     }
-
-    for (int j = i - 1; j >= 0; j--) {
-        printf("%d", binaryNum[j]);
-    }
+    printf("\n");
 }
 
-int main() {
+int main()
+{
     int n;
-    printf("Enter a decimal number: ");
+
+    printf("Enter a Decimal number: ");
     scanf("%d", &n);
-    printf("Binary equivalent: ");
-    decimalToBinary(n);
-    printf("\n");
+
+    printf("The Binary equivalent of %d = ", n);
+    toBinary(n);
     return 0;
 }
