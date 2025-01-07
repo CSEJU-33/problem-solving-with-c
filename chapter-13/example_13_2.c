e3fa65949e93449d06b3b9367eb1718101b82557
Maliha Laheen <laheenmaliha@gmail.com>
2025-01-07 11:40:26 +0600

    added example13_2

---
+++ chapter-13/example_13_2.c
@@ -0,0 +1,18 @@
+//Example 13.1 To illustrate the concept of pointer arithmetic
+#include<stdio.h>
+int main()
+{
+    int i, *ip; 
+    ip=&i;
+    printf("ip=%u\n", ip);
+    ip++;
+    printf("After ip++    ip=%u\n", ip);
+    ip--;
+    printf("After ip--    ip=%u\n", ip);
+    ip=ip+2;
+    printf("After ip=ip+2    ip=%u\n", ip);
+    ip=ip-2;
+    printf("After ip=ip-2    ip=%u\n", ip);
+    
+    return(0);
+}
